01067852e00556793a1bf227c640404c83b86d5a
adonismolina03 <adonismolina03@gmail.com>
2023-08-21 08:59:13 -0600

    Inicio de prueba tecnica 02

---
+++ AAMG20230821.EntidadesDeNegocio/src/aamg20230821/entidadesdenegocio/AAMG20230821EntidadesDeNegocio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aamg20230821.entidadesdenegocio;
+
+/**
+ *
+ * @author Adonis Molina
+ */
+public class AAMG20230821EntidadesDeNegocio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
